25d9c025628300794b9e9c10c6d8dcd8a3dbc50a
fabimartinez556 <fabimartinez556@gmail.com>
2026-01-18 22:47:01 -0300

    mejora en ios

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,9 +1,14 @@
-
 // src/firebase/firebaseConfig.js
+import { Platform } from "react-native";
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGdeZyGyRnYE_mYRMcrJcypGJBOo5Ooyg",
@@ -15,8 +20,16 @@ const firebaseConfig = {
   databaseURL: "https://react-app-8a129-default-rtdb.firebaseio.com",
 };
 
+// evita duplicate-app en HMR
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// ✅ Auth: web normal, native con persistencia real
+export const auth =
+  Platform.OS === "web"
+    ? getAuth(app)
+    : initializeAuth(app, {
+        persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+      });
+
 export const db = getFirestore(app);
 export const realtimeDb = getDatabase(app);
